a62c917ffe7561cf80a3bc280d2288f793396c97
ViniciusCardoso1 <vinibitencourt83@gmail.com>
2025-03-24 20:29:48 -0300

    Adicionado as alterações requisitadas

--- hello-world.js
+++ hello-world.js
@@ -1,12 +1,39 @@
-const nameInput = document.getElementById('name');
-const messageP = document.getElementById('message');
+document.addEventListener("DOMContentLoaded", function() {
+    const nameInput = document.getElementById("nameInput");
+    const helloButton = document.getElementById("helloButton");
+    const clearButton = document.getElementById("clearButton");
+    const message = document.getElementById("message");
 
-nameInput.onkeyup = (event) => {
-    if (nameInput.value === '') {
-        messageP.innerText = '';
-    }
-}
+    nameInput.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            const name = nameInput.value;
+            if (name) {
+                message.textContent = `Hello World, ${name}!`;
+            } else {
+                message.textContent = "Por favor, digite seu nome!";
+            }
+        }
+    });
 
-const helloWorld = () => {
-    messageP.innerText = `Hello World, ${nameInput.value}!`;
-}
+    nameInput.addEventListener("keydown", function(event) {
+        if (event.key === "Escape") {
+            nameInput.value = ""; 
+            message.textContent = "";
+        }
+    });
+
+
+    clearButton.addEventListener("click", function() {
+        nameInput.value = "";
+        message.textContent = "";
+    });
+
+    helloButton.addEventListener("click", function() {
+        const name = nameInput.value;
+        if (name) {
+            message.textContent = `Hello World, ${name}!`;
+        } else {
+            message.textContent = "Por favor, digite seu nome!";
+        }
+    });
+});
